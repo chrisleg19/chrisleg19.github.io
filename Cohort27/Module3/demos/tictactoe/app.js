1f5ee3d65eeab50a7311393f6a55a38cace72425
chrisleg19 <110962234+chrisleg19@users.noreply.github.com>
2022-10-19 16:15:58 -0400

    update

--- Cohort27/Module3/demos/tictactoe/app.js
+++ Cohort27/Module3/demos/tictactoe/app.js
@@ -1,12 +1,19 @@
 //Note: we are not importing separate components because we are building each one in the app.js file
 //Note: we are not importing the CSS file (main.css) because it is called in the index.html file.  The index.html file is connected to the app.js file through: ReactDOM.render(<App/>, document.getElementById("root"))
 
+//Inside the components (inside return()), you can use HTML and JSX
+
+
+
+
 
 //Header Component
-const Header = () =>{
+//(props) now allows the Header component to take in props
+const Header = (props) =>{
+    console.log(props)
     return(
         <div>
-            <h1>React Tic Tac Toe</h1>
+            <h1>{props.msg}</h1>
         </div>
     )
 }
@@ -47,13 +54,31 @@ const Square = (props) =>{
     )
 }
 
+const Navbar = (props) =>{
+    console.log(props)
+    return(
+        <nav>
+            <a>Home</a>
+        </nav>
+    )
+}
+
+
+
+
 //App Component
+
 //All components should be rendered to the App component.  The App component is then rendered to the "root" div (id="root") in the .html file.  This is done through: ReactDOM.render(<App/>, document.getElementById("root"))
 
+//NOTE: Basic way to pass props is through the parent component: App
 const App = () =>{
+    // Example of creating a variable and passing it to the Header and Navbar as a prop.  The prop is called in the Header component and the Navbar component.
+    const message = "React Tic Tac Toe"
+    //below message1 & msg are the keys, and {message} is the value
     return(
         <div>
-            <Header/>
+            <Navbar message1={message}/>
+            <Header msg={message}/>
             <Player whichPlayer='X'/>
             <Player whichPlayer='O'/>
             <Board/>
